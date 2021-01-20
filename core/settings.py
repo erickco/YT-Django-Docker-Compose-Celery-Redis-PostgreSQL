e0f45f1baea5ab1354242bc96fa2c04719782ec4
erickco <ericcoetzee@gmail.com>
2021-01-20 16:30:35 +0000

    Updated to fetch the correct env variable

--- core/settings.py
+++ core/settings.py
@@ -122,4 +122,4 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 CELERY_BROKER_URL = os.environ.get("CELERY_BROKER", "redis://redis:6379/0")
-CELERY_RESULT_BACKEND = os.environ.get("CELERY_BROKER", "redis://redis:6379/0")
+CELERY_RESULT_BACKEND = os.environ.get("CELERY_BACKEND", "redis://redis:6379/0")
